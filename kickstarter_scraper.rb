7eb4571bd0f34d9d85df10deb421532a656d1ba2
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-02 16:04:06 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta li.a.location-name")
+# location: project.css("ul.project-meta li.a.location-name").text
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
